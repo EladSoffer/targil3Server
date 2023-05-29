90b030ab8efd90d6065732c82d83c0a2ad6136c7
yuvalavik <116814174+yuvalavik@users.noreply.github.com>
2023-05-29 19:27:10 +0300

    changes in app.js

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 const express = require('express');
-var app = express();
+const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-server.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-const costumEnv = require('custom-env');
-costumEnv.env(process.env.NODE_ENV, './config');
-
+const customEnv = require('custom-env');
+customEnv.env(process.env.NODE_ENV, './config');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+const mongoURI = 'mongodb://127.0.0.1:27017/mydatabase';
 
-
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(8080, () => {
+      console.log('Server started on port 8080');
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 const messages = require('./routes/message');
-
-app.use('./api/Chats/:id/Messages', messages)
+app.use('/api/Chats/:id/Messages', messages);
 
 const register = require('./routes/register');
-app.use('./api/Users', register)
+app.use('/api/Users', register);
 
-const login = require('/routes/login');
-app.use('/api/Chats/:id/Messages/', messages);
-app.use('/api/Tokens',login);
-
+const login = require('./routes/login');
+app.use('/api/Tokens', login);
 
 app.set('view engine', 'ejs');
-
-
-app.listen(8080)
